7bf9416985b40beb4cfe62ce47c573486bbcf115
Nejc Kisek <nejc@kisek.si>
2017-11-17 18:32:02 +0100

    1.0.2: ime baze, spremenjen CMD v dockerju

--- startdb.bash
+++ startdb.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name rsobook_db_images -e POSTGRES_USER=dbuser -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=images -p 5432:5432 postgres:latest
+docker run -d --name rsobook_db_images -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root -e POSTGRES_DB=rsobook-image -p 5432:5432 postgres:latest
